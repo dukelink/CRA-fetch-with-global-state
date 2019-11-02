2517d0c5f0f12418c7895cc77435e194f23fdec9
dukelink <duke.lotherington@gmail.com>
2019-11-02 09:54:47 -0700

    Add setState hook to SFC

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/*
+function getCount() {
+const [count, setCount] = useState(0);
+}
+*/
+
 const App: React.FC = () => {
+
+  const [ count, setCount ] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload. # 
         </p>
         <a
           className="App-link"
@@ -18,9 +27,22 @@ const App: React.FC = () => {
         >
           Learn React
         </a>
+
+        <button onClick={ runSQL }>Read SQL</button>
+
+        <button onClick={ ()=>{setCount(count+1)} }> { 'Count: '+count } </button>
       </header>
     </div>
   );
+}
+
+function runSQL() {
+
+  fetch('http://localhost:8080/api/statuses')
+    .then(res => res.json())
+    .then(res => alert(JSON.stringify(res)));
+
+//  setCount(count+1);
 }
 
 export default App;
